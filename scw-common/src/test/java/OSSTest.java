5d3edb6d73ed8c2ef2857ab9537fdf6749113478
huangzuyong <76094120+huangzuyong@users.noreply.github.com>
2021-07-10 10:38:04 +0800

    Update OSSTest.java

--- scw-common/src/test/java/OSSTest.java
+++ scw-common/src/test/java/OSSTest.java
@@ -11,8 +11,8 @@ public class OSSTest {
         // Endpoint以北京为例，其它Region请按实际情况填写。
         String endpoint = "http://oss-cn-beijing.aliyuncs.com";
         // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
-        String accessKeyId = "LTAI4GDeDtojLdVugqhfdXjo";
-        String accessKeySecret = "MICx2eUOjvWwvvtOHhG0rNkjaZuiM0";
+        String accessKeyId = "LTAI4GDeDtojLd56yyhXjo";
+        String accessKeySecret = "MICx2eUOjvWwvvtfghjkhgfdsfghiM0";
         // 创建OSSClient实例。
         OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
         // 上传文件流。
